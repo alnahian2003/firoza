592f3e2552203967e7ef499548e537f62e3446ce
Al Nahian <a.alnahian2003@gmail.com>
2024-06-03 04:22:40 +0600

    filtering posts by category

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,9 +15,16 @@ class HomeController extends Controller
     public function __invoke(Request $request)
     {
         $posts = Post::when($request->query('search'), function (Builder $query) use ($request) {
-            return $query->where('title', 'LIKE', '%' . $request->query('search') . '%')
-                ->orWhere('body', 'LIKE', '%' . $request->query('search') . '%');
+            return $query->where('title', 'LIKE', '%'.$request->query('search').'%')
+                ->orWhere('body', 'LIKE', '%'.$request->query('search').'%');
         })
+            ->when($request->query('category'), function (Builder $query) use ($request) {
+                // return $query->whereHas('category', function (Builder $query) use ($request) {
+                //     return $query->where('slug', $request->query('category'));
+                // });
+
+                return $query->whereRelation('category', 'slug', $request->query('category'));
+            })
             ->latest()
             ->paginate(10);
 
